73985e15b2da67a34afbde28cdd0574225aba70b
--replace-all <paragunhale1998@gmail.com>
2023-06-11 21:50:32 +0530

    learning useContext

---
+++ React-Hooks/src/useContext/TheamContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const TheamContext = createContext('darkMode')
+
+export default TheamContext
---
+++ React-Hooks/src/useContext/VideoContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const videoContext = createContext(null)
+
+export default videoContext
